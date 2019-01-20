ffd59f653b0575d695bbeb7021f0a9bfc9edbef3
AbdulBasit KABIR <abdulbasitkay@gmail.com>
2019-01-20 02:33:50 +0100

    fix tf

--- deploy.tf
+++ deploy.tf
@@ -77,7 +77,7 @@ resource "aws_instance" "http-echo" {
       "sudo yum -y update",
       "sudo yum -y install docker",
       "sudo usermod -aG docker $USER",
-      "sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/""docker-compose",
+      "sudo curl -L \"https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/""docker-compose",
       "sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose",
       "sudo docker-compose --version",
       "git clone https://github.com/abdulbasitkay/terraform-aws-sample.git",
