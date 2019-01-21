2685d4255595b23c2ad5774aa3de053ba1aa5f57
AbdulBasit KABIR <abdulbasitkay@gmail.com>
2019-01-21 12:34:11 +0100

    fix tf

--- deploy.tf
+++ deploy.tf
@@ -68,19 +68,7 @@ resource "aws_instance" "http-echo" {
 
   key_name = "${aws_key_pair.auth.id}"
   vpc_security_group_ids = ["${aws_security_group.defaultsg.id}"]
-  user_data = <<-EOF
-              #!/usr/bin/env bash
-              sudo yum -y update
-              sudo yum -y install docker
-              sudo usermod -aG docker $USER
-              sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-              sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-              sudo docker-compose --version
-              git clone https://github.com/abdulbasitkay/terraform-aws-sample.git
-              cd terraform-aws-sample
-              sudo docker-compose up -d
-              EOF
-
+  user_data = "${file("./data.sh")}"
 }
 
 output "address" {
