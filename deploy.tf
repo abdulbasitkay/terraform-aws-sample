a5d7db0c5830fa1b937f017b882f26ff60aac6a8
AbdulBasit KABIR <abdulbasitkay@gmail.com>
2019-01-21 10:42:06 +0100

    fix tf

--- deploy.tf
+++ deploy.tf
@@ -49,14 +49,14 @@ resource "aws_security_group" "defaultsg" {
   }
 }
 
-resource "aws_key_pair" "auth" {
-  key_name   = "abulkay"
-  public_key = "${file("./abulkay.pub")}"
-}
+#resource "aws_key_pair" "auth" {
+  #key_name   = "abulkay"
+  #public_key = "${file("./abulkay.pub")}"
+#}
 
 resource "aws_instance" "http-echo" {
   connection {
-    type     = "ssh"
+    #type     = "ssh"
     # username for our AMI
     user = "ec2-user"
 
@@ -66,7 +66,7 @@ resource "aws_instance" "http-echo" {
   ami           = "ami-01e24be29428c15b2"
   instance_type = "t2.micro"
 
-  key_name = "${aws_key_pair.auth.id}"
+  #key_name = "${aws_key_pair.auth.id}"
   vpc_security_group_ids = ["${aws_security_group.defaultsg.id}"]
   user_data = ${file("data.sh")}
 
